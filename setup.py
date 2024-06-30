e031aa966c3e696b60ca3e74d2e6b174924ef923
Kort Driessene <driessenkort@gmail.com>
2024-06-30 13:55:40 -0500

    create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="ssfm",
+    version="0.1",
+    description="Automated sleepscoring of chronic electrophysiology data",
+    url="http://github.com/kortdriessen/ssfm",
+    author="Kort Driessen",
+    author_email="driessen2@wisc.edu",
+    license="MIT",
+    packages=["ssfm"],
+    install_requires=[
+    ],
+    zip_safe=False,
+)
